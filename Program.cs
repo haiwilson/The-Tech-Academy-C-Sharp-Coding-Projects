f25f06ce5dff467a5f920c86220c09414e97c8dc
haiwilson <52790398+haiwilson@users.noreply.github.com>
2019-09-11 17:33:01 -0400

    daily report program

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programcs
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("The Tech Academy");
+			Console.WriteLine("Student Daily Report");
+			Console.WriteLine("What Course are you on?");
+			string courseNumber = Console.ReadLine();
+			Console.WriteLine("What Page Number?");
+			string pageNumber = Console.ReadLine();
+			Console.WriteLine("Do you need help with anything? Please say Yes/No");
+			string helpAnswer = Console.ReadLine();
+			Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+			string posExperience = Console.ReadLine();
+			Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+			string proFeedback = Console.ReadLine();
+			Console.WriteLine("How many hours did you study today?");
+			string studyHours = Console.ReadLine();
+			Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly.Have a great day!");
+			Console.ReadLine();
+			Console.WriteLine("You are on Course Number " + courseNumber + "  and one Page Number " + pageNumber + "  and you studied for " + studyHours + " hours");
+			Console.ReadLine();
+		}
+	}
+}
