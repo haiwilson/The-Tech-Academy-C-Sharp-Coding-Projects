e9eecfb2ef9fc4402267ed7c171f4c3ee99ea6eb
haiwilson <52790398+haiwilson@users.noreply.github.com>
2019-09-16 16:32:56 -0400

    income of 2 weekly salaried persons

---
+++ income/income/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace income
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Anonymous Income Comparison Program");
+			Console.WriteLine("Person 1");
+			Console.WriteLine("What is the hourly rate?");
+			int personSal1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Hours worked per week?");
+			int personHrs1 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Person 2");
+			Console.WriteLine("What is the hourly rate?");
+			int personSal2 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Hours worked per week?");
+			int personHrs2 = Convert.ToInt32(Console.ReadLine());
+
+			int weeklySal1 = personSal1 * personHrs1;
+			Console.WriteLine("Weekly salary of Person 1:" + weeklySal1.ToString());
+			Console.ReadLine();
+
+			int weeklySal2 = personSal2 * personHrs2;
+			Console.WriteLine("Weekly salary of Person 2:" + weeklySal2.ToString());
+			Console.ReadLine();
+		}
+	}
+}
