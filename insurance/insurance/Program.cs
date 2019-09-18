ac50abc5732ad1b0a78581418fb4b4b9fa39cab8
haiwilson <52790398+haiwilson@users.noreply.github.com>
2019-09-18 09:06:28 -0400

    insurance eligibility program

---
+++ insurance/insurance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insurance program");
+            Console.WriteLine("What is your age?");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had DUI?");
+            bool input2 = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+            bool isQualified = (input1 > 15 && input2 == false && input3 < 4);
+            Console.WriteLine(isQualified);
+            Console.ReadLine();
+        }
+    }
+}
