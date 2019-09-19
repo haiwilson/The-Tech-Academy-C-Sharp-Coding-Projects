81a770137fec32092314861b9a6c0a2aa365c97b
haiwilson <52790398+haiwilson@users.noreply.github.com>
2019-09-19 12:20:12 -0400

    boolean comparison using a while  and do while statements

---
+++ booleancomp/booleancomp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booleancomp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Value of i: {0}", i);
+                i++;
+            } while (i < 10);
+            Console.ReadLine();
+
+            int j = 0;
+
+            while (j < 10)
+            {
+                Console.WriteLine("Value of j: {0}", j);
+
+                j++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
