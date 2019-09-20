1b1247f34f795beaa637f52be9084b82a62e6b06
haiwilson <52790398+haiwilson@users.noreply.github.com>
2019-09-20 11:08:20 -0400

    stringbuilder examples uploaded

---
+++ stringbuilder/stringbuilder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "Wilson";
+            string str = "Hello " + userName + "." + "Hope you have a good day!" + ".";
+            Console.WriteLine(str);
+            Console.ReadLine();
+
+            string str2 = "Today is a good day";
+            string upper2 = str2.ToUpper();
+            Console.WriteLine(upper2);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Wilson. ");
+            sb.Append("Today is a good day, ");
+            sb.Append("Hope you're having fun! ");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
