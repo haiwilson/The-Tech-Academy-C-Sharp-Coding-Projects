bda4042d17d002c0037b70f35ed4140a83f2d864
haiwilson <52790398+haiwilson@users.noreply.github.com>
2019-09-27 10:48:23 -0400

    created methodclass as part of step111

---
+++ methodclass/methodclass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input value of x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input value of y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Math a = new Math();
+            int add = a.AddTwoValues(x, y);
+            int sub = a.SubTwoValues(x, y);
+            int mul = a.MulTwoValues(x, y);
+
+            Console.WriteLine("X + Y = " + add);
+            Console.WriteLine("X - Y = " + sub);
+            Console.WriteLine("X * Y = " + mul);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodclass/methodclass/addition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodclass
+{
+    class Math
+
+        {
+        public int AddTwoValues(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int SubTwoValues(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int MulTwoValues(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
