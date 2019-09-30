052c531dd0399360a5d4b7c9d03e31c97591555f
haiwilson <52790398+haiwilson@users.noreply.github.com>
2019-09-30 14:58:59 -0400

    method math class example uploaded

---
+++ methodmath/methodmath/Div.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodmath
+{
+    class Div
+    {
+        double x;
+        public double DivByTwo(int y)
+        {
+            return x = y / 2;
+        }
+    }
+}
---
+++ methodmath/methodmath/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodmath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input value of Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            
+            Div a = new Div();
+            double x = a.DivByTwo(y);
+           
+            Console.WriteLine("Value of input Y divided by 2 is =  " + x);
+            Console.ReadLine();
+
+        }
+    }
+}
